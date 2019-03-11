f3436d0977f4ed11a410d62c3d8844fbb4f710df
KPHarmon <kade_harmon@yahoo.com>
2019-03-11 13:47:53 -0700

    eth fix

--- networking/ethernet.py
+++ networking/ethernet.py
@@ -1,6 +1,5 @@
 import socket
 import struct
-from general import *
 
 
 class Ethernet:
@@ -9,8 +8,6 @@ class Ethernet:
 
         dest, src, prototype = struct.unpack('! 6s 6s H', raw_data[:14])
 
-        self.dest_mac = get_mac_addr(dest)
-        self.src_mac = get_mac_addr(src)
         self.proto = socket.htons(prototype)
         self.data = raw_data[14:]
 
